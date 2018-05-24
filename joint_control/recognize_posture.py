107bd280a1fb4b6c33c4584b777572f5ba48ab19
hackelle <leohackel@hotmail.de>
2018-05-24 10:43:32 +0200

    recongnize posture runs

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -11,8 +11,8 @@
 
 
 from angle_interpolation import AngleInterpolationAgent
-from keyframes import hello
-import pikle
+from keyframes import hello, leftBackToStand, leftBellyToStand, rightBackToStand, rightBellyToStand, wipe_forehead
+import pickle
 
 ROBOT_POSE_CLF = "robot_pose.pkl"
 
@@ -24,7 +24,7 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = pikle.load(open(ROBOT_POSE_CLF))  # LOAD CLASSIFIER
+        self.posture_classifier = pickle.load(open(ROBOT_POSE_CLF))  # LOAD CLASSIFIER
 
     def think(self, perception):
         self.posture = self.recognize_posture(perception)
@@ -32,11 +32,30 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
 
     def recognize_posture(self, perception):
         posture = 'unknown'
-        # YOUR CODE HERE
+        
+        # collect data
+        data_strings = ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch']
+        data = []
+        for name in data_strings:
+            data.append(perception.joint[name])
+            
+        data.append(perception.imu[0])
+        data.append(perception.imu[1])
+        
+        prediction_index = self.posture_classifier.predict([data])[0] # needs 2d array, give it an array and take index 0
+        # get prediction from given list
+        posture = ['Right', 'Back', 'Stand', 'Belly', 'HeadBack', 'Sit', 'Knee', 'Left', 'Frog', 'Crouch', 'StandInit'][prediction_index]
+
+        print posture
 
         return posture
 
 if __name__ == '__main__':
     agent = PostureRecognitionAgent()
-    agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    #agent.keyframes = hello()  # CHANGE DIFFERENT KEYFRAMES
+    agent.keyframes = leftBackToStand()
+    #agent.keyframes = leftBellyToStand()
+    #agent.keyframes = rightBackToStand()
+    #agent.keyframes = rightBellyToStand()
+    #agent.keyframes = wipe_forehead(1)
     agent.run()
